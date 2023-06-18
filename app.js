93709bb9a1c9850584a649a18da8d0f0509bcb39
xvolld <136752887+xvolld@users.noreply.github.com>
2023-06-19 01:07:24 +0330

    Update app.js
    test

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ const songs = [
 
 
 
-Array.from(Document.getElementsByClassName('songItem')).forEach((e, i,) =>{
-    e.getElementsByTagName('img')[0].src = songs[i].poster;
-    e.getElementsByTagName('h5')[0].innerHTML = songs[i].songName;
+// Array.from(Document.getElementsByClassName('songItem')).forEach((e, i,) =>{
+//     e.getElementsByTagName('img')[0].src = songs[i].poster;
+//     e.getElementsByTagName('h5')[0].innerHTML = songs[i].songName;
 
-});
+// });
 
 
 let masterPlay = document.getElementById('masterPlay');
